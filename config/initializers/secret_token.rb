e182f4aef1402ca9814660c233be3e3fae743995
Eric Boehs <ericboehs@gmail.com>
2010-09-02 14:03:51 -0500

    Initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dailydeals::Application.config.secret_token = 'b2d7052b45fc3192516e6226f373e2aaa754c25c72512d6025977124be2abdee325ec8ada3a7d4ca9a61778a7ca151a87a6fcc2f736d36825a8172bf64d02c6c'
